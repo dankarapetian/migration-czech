d2124864bf12c27588d906bf6f968ab2c9cc858a
dankarapetian <91783477+dankarapetian@users.noreply.github.com>
2025-05-08 14:28:24 +0200

    Create people.js

---
+++ people.js
@@ -0,0 +1,22 @@
+const people = {
+  "ramil alekberov": {
+    dob: "20.09.1998",
+    birthplace: "Moskva / Rusko",
+    sex: "M",
+    nationality: "Česká republika",
+    issued: "27.03.2025",
+    expires: "27.03.2035",
+    doc: "283359730",
+    photo: "ramil.jpg"
+  },
+  "natalia garinova": {
+    dob: "15.09.1995",
+    birthplace: "Jaroslavl / Rusko",
+    sex: "F",
+    nationality: "Česká republika",
+    issued: "27.01.2025",
+    expires: "27.01.2035",
+    doc: "260735219",
+    photo: "natalia.jpg"
+  }
+};
